29d8be1a78d9589ae426dcb7bc3b1390797f751e
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-06-25 12:55:58 +0000

    learning Node.js

--- createFile.mjs
+++
@@ -1,7 +0,0 @@
-import {writeFile }from 'node:fs';
-
-writeFile('./hello.html','<h1>Learning Node.js</h1>',()=>{
-    console.log('File Added');
-
-})
-console.log('Learning ')
--- deleteFile.mjs
+++
@@ -1,5 +0,0 @@
-import {unlink}from 'node:fs';
-
-unlink('./hello.html',() => {
-    console.log('File Deleted');
-});
